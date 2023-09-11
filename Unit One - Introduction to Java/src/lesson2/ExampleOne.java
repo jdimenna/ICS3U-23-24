7e180450954c436cb9dc5e06fbea3cf0cc6b234a
Juliet DiMenna <jdimenna@bayviewglen.ca>
2023-09-11 10:57:31 -0400

    Changed Example One

--- Unit One - Introduction to Java/src/lesson2/ExampleOne.java
+++ Unit One - Introduction to Java/src/lesson2/ExampleOne.java
@@ -4,5 +4,6 @@ public class ExampleOne {
     public static void main(String[] args) {
         //sysout
         System.out.println("Hello ICS3U!");
+        System.out.println("Goodbye ICS3U!");
     }
 }
