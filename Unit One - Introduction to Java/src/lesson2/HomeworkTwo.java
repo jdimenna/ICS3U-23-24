9fd378398e02d88558816d9efb36b7a47a6c18b2
Juliet DiMenna <jdimenna@bayviewglen.ca>
2023-09-11 11:21:17 -0400

    homework two

---
+++ Unit One - Introduction to Java/src/lesson2/HomeworkTwo.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+public class HomeworkTwo {
+    public static void main(String[] args) {
+        System.out.println("Presentation: Exploring Huron-Wendat Heritage");
+        System.out.println("Aspects of Huron-Wendat Culture:");
+        System.out.println("- Traditional Practices");
+        System.out.println("- Arts and Craftsmanship");
+        System.out.println("- Language and Storytelling");
+        System.out.println("- Community Traditions");
+        System.out.println("Date: Wednesday, November 15, 2023");
+        System.out.println("Duration: 60 minutes");
+    }
+}
