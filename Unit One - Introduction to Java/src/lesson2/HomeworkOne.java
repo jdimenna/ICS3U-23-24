801b28313841e9cec9b3070e9a08763f8856ad9f
Juliet DiMenna <jdimenna@bayviewglen.ca>
2023-09-11 11:16:56 -0400

    homework 1

---
+++ Unit One - Introduction to Java/src/lesson2/HomeworkOne.java
@@ -0,0 +1,12 @@
+package lesson2;
+
+public class HomeworkOne {
+    public static void main(String[] args) {
+        System.out.println("Question: What country produces the most coffee?");
+        System.out.println("A: Italy");
+        System.out.println("B: Colombia");
+        System.out.println("C: China");
+        System.out.println("D: Brazil");
+        System.out.println("Correct Answer: D) Brazil");
+    }
+}
